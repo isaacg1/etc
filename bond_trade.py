02eb67d26350307354f8dde87e786b202b6b1e9b
Isaac Grosof <isaacg@mit.edu>
2016-07-21 11:42:48 -0400

    a

--- bond_trade.py
+++ bond_trade.py
@@ -3,15 +3,26 @@ from library import *
 BOND_PRICE = 1000
 BOND_ALLOWED = 100
 
-def bond_trade(book, position):
-    if book[BOND].other.bid:
-        best_bid = book[BOND].other.buy[0]
-        if best_bid > BOND_PRICE:
-            max_allowed = position[BOND] - BOND_ALLOWED
+position = 0
+
+def bond_trade(book):
+    global position
+    if book[SYMBOL] != BOND:
+        return []
+    other = book[BUY]
+    if other:
+        best_bid = other[0]
+        if best_bid[0] > BOND_PRICE:
+            max_allowed = min(position - BOND_ALLOWED, best_bid[1])
             id, order = create_add_order(BOND, SELL, BOND_PRICE, max_allowed)
             out.append(id, order)
-    if book[BOND].other.offer:
-        best_offer = book[BOND].other.offer[0]
-        if best_offer < BOND_PRICE:
-            max_allowed = BOND_Allowed - position[BOND]
+            position -= max_allowed
+    other = book[SELL]
+    if other:
+        best_offer = other[0]
+        if best_offer[0] < BOND_PRICE:
+            max_allowed = min(BOND_ALLOWED - position[BOND], best_offer[1])
             id, order = create_add_order(BOND, BUY, BOND_PRICE, max_allowed)
+            out.append(id, order)
+            position += max_allowed
+    return out
