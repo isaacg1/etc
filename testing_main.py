a42be8b19794b27d37adeef9203205534c8333c2
Ubuntu <ubuntu@bot-jiffy.js.js-etc>
2016-07-21 16:48:34 +0000

    s

--- testing_main.py
+++ testing_main.py
@@ -1,13 +1,15 @@
+from __future__ import print_function
 from library import *
 import bond_trade
+import sys
 
 def main():
     connect_to_test()
     send_message(create_buy_order(999999, BOND,  1, 999))
     while True:
         msg = get_message()
-        print("got msg")
-        print(json.dumps(msg))
+        print("got msg", file=sys.stderr)
+        print(json.dumps(msg), file=sys.stderr)
         bond_trade.bond_trade(msg)
-        print("bond_trade finished")
+        print("bond_trade finished", file=sys.stderr)
 main()
