3176ec5062dc4cc4570d15aadd62567aa4f87a5f
Ubuntu <ubuntu@bot-jiffy.js.js-etc>
2016-07-21 20:29:25 +0000

    val_trade

--- trade.py
+++ trade.py
@@ -2,8 +2,8 @@ from library import *
 from bond_trade import start, bond_trade2
 import val_trade
 
-start_funcs = [start]
-message_reactions= [bond_trade2, val_trade.penny_valbz]
+start_funcs = []
+message_reactions= [val_trade.penny_valbz]
 
 def start_trading():
     for f in start_funcs:
