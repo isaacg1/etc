639deb53a550c7d1bd6d4d388560e0f3af8658cd
Michał Glapa <michal.glapa@gmail.com>
2016-07-21 19:27:59 -0400

    l

--- library.py
+++ library.py
@@ -15,6 +15,10 @@ XLF = "XLF"
 EXCHANGE = 0
 _ID = 0
 
+
+id_to_symbol_map = {}
+
+
 def get_new_id():
     global _ID
     _ID += 1
@@ -27,11 +31,11 @@ def create_add_order(symbol, buy_or_sell, size, price):
     order = {"type": "add", "order_id": idd, "symbol": symbol, "dir": buy_or_sell, "price": price, "size": size}
     return json.dumps(order), idd
 
-def create_buy_order(symbol, quantity, price):
-    return create_add_order(symbol, BUY, quantity, price)
+def create_buy_order(symbol, size, price):
+    return create_add_order(symbol, BUY, size, price)
 
-def create_sell_order(symbol, quantity, price):
-    return create_add_order(symbol, SELL, quantity, price)
+def create_sell_order(symbol, size, price):
+    return create_add_order(symbol, SELL, size, price)
 
 def connect_to_test():
     print("CONNECTING TO TEST EXCHANGE")
@@ -54,6 +58,18 @@ def connect_to_prod():
 def send_message(order):
     print("->" + order, file=sys.stderr)
     print(order, file=EXCHANGE)
+
+def send_sell_order(symbol, size, price):
+    order, id = create_sell_order(symbol, size, price)
+    id_to_symbol_map[id] = (symbol, size, price, SELL)
+    send_message(order)
+    return id
+
+def send_buy_order(symbol, size, price):
+    order, id = create_buy_order(symbol, size, price)
+    id_to_symbol_map[id] = (symbol, size, price, BUY)
+    send_message(order)
+    return id
 
 def get_message():
     s = EXCHANGE.readline().strip()
