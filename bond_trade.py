c97d2d494d5b6ca40d08cfecbf64e5058fb85c84
Isaac Grosof <isaacg@mit.edu>
2016-07-21 12:27:16 -0400

    a

--- bond_trade.py
+++ bond_trade.py
@@ -15,27 +15,30 @@ id = 0
 def bond_trade(book):
     global position
     global id
-    if book[TYPE] != BOOK:
+    if book[TYPE] == BOOK and book[SYMBOL] == BOND:
+        other = book[BUY]
+        if other:
+            best_bid = other[0]
+            if best_bid[0] > BOND_PRICE:
+                max_allowed = min(position - BOND_ALLOWED, best_bid[1])
+                if max_allowed > 0:
+                    order = create_sell_order(id, BOND, BOND_PRICE, max_allowed)
+                    id += 1
+                    send_message(order)
+                    print("sent order to buy %s", max_allowed)
+        other = book[SELL]
+        if other:
+            best_offer = other[0]
+            if best_offer[0] < BOND_PRICE:
+                max_allowed = min(BOND_ALLOWED - position, best_offer[1])
+                if max_allowed > 0:
+                    order = create_buy_order(id, BOND, BOND_PRICE, max_allowed)
+                    id += 1
+                    send_message(order)
+                    print("sent order to sell %s", max_allowed)
         return
-    if book[SYMBOL] != BOND:
-        return
-    other = book[BUY]
-    if other:
-        best_bid = other[0]
-        if best_bid[0] > BOND_PRICE:
-            max_allowed = min(position - BOND_ALLOWED, best_bid[1])
-            order = create_sell_order(id, BOND, BOND_PRICE, max_allowed)
-            id += 1
-            send_message(order)
-            print("sent order to buy %s", max_allowed)
-            position -= max_allowed
-    other = book[SELL]
-    if other:
-        best_offer = other[0]
-        if best_offer[0] < BOND_PRICE:
-            max_allowed = min(BOND_ALLOWED - position, best_offer[1])
-            order = create_buy_order(id, BOND, BOND_PRICE, max_allowed)
-            id += 1
-            send_message(order)
-            print("sent order to sell %s", max_allowed)
-            position += max_allowed
+    if book[TYPE] == FILL and book[SYMBOL] == BOND:
+        if book[DIR] == BUY:
+            position += book[SIZE]
+        if book[DIR] == SELL:
+            position -= book[SIZE]
