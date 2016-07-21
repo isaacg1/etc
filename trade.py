e6b5c0f1d7adfc692fe612c7ec866564b3b32faa
Ubuntu <ubuntu@bot-jiffy.js.js-etc>
2016-07-21 21:32:55 +0000

    better

--- trade.py
+++ trade.py
@@ -8,9 +8,10 @@ import wfc_penny
 import xlf_penny
 import adr_trade
 
-start_funcs = [adr_trade.start]
-message_reactions= [adr_trade.trade]
-
+start_funcs = [start]
+message_reactions= [val_trade.penny_valbz, bond_trade2,
+        valbz_trade.penny_valbz, ms_penny.penny_valbz, gs_penny.penny_valbz,
+        wfc_penny.penny_valbz, xlf_penny.penny_valbz]
 def start_trading():
     for f in start_funcs:
         f()
