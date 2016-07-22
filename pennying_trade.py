80cbbd2895e1cee1be31e19fba2e892b15103c36
Ubuntu <ubuntu@bot-jiffy.js.js-etc>
2016-07-22 19:13:58 +0000

    s

--- pennying_trade.py
+++ pennying_trade.py
@@ -4,23 +4,24 @@ from penny import create_penny
 LOW_BETA_SPREAD = 15
 NORMAL_BETA_SPREAD = 20
 HIGH_BETA_SPREAD = 25
+LIQUID_SPREAD = 10
 
-ETF_SIZE = 3
+ETF_SIZE = 7
 
 etf_pennies = [ create_penny(library.XLY, HIGH_BETA_SPREAD, ETF_SIZE),
                 create_penny(library.XLP, NORMAL_BETA_SPREAD, ETF_SIZE),
                 create_penny(library.XLU, LOW_BETA_SPREAD, ETF_SIZE),
-                create_penny(library.RSP, HIGH_BETA_SPREAD, ETF_SIZE)
+                create_penny(library.RSP, 50, ETF_SIZE)
                 ]
 
-STOCK_SIZE = 3
-stock_pennies = [create_penny(library.AMZN, HIGH_BETA_SPREAD, STOCK_SIZE),
+STOCK_SIZE = 7
+stock_pennies = [create_penny(library.AMZN, LIQUID_SPREAD, STOCK_SIZE),
                  create_penny(library.HD, HIGH_BETA_SPREAD, STOCK_SIZE),
                  create_penny(library.DIS, HIGH_BETA_SPREAD, STOCK_SIZE),
-                 create_penny(library.PG, NORMAL_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.PG, LIQUID_SPREAD, STOCK_SIZE),
                  create_penny(library.KO, NORMAL_BETA_SPREAD, STOCK_SIZE),
                  create_penny(library.PM, NORMAL_BETA_SPREAD, STOCK_SIZE),
-                 create_penny(library.NEE, LOW_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.NEE, LIQUID_SPREAD, STOCK_SIZE),
                  create_penny(library.DUK, LOW_BETA_SPREAD, STOCK_SIZE),
                  create_penny(library.SO, LOW_BETA_SPREAD, STOCK_SIZE)]
 
