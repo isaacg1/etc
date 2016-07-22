f639e55785e81bd552ef5104c0cd004060a8efa4
Ubuntu <ubuntu@bot-jiffy.js.js-etc>
2016-07-22 18:11:45 +0000

    s

--- pennying_trade.py
+++ pennying_trade.py
@@ -1,27 +1,28 @@
 import library
 from penny import create_penny
 
-ETF_SPREAD = 22
-STOCK_SPREAD = 22
+LOW_BETA_SPREAD = 20
+NORMAL_BETA_SPREAD = 28
+HIGH_BETA_SPREAD = 36
 
 ETF_SIZE = 8
 
-etf_pennies = [ create_penny(library.XLY, ETF_SPREAD, ETF_SIZE),
-                create_penny(library.XLP, ETF_SPREAD, ETF_SIZE),
-                create_penny(library.XLU, ETF_SPREAD, ETF_SIZE),
+etf_pennies = [ create_penny(library.XLY, HIGH_BETA_SPREAD, ETF_SIZE),
+                create_penny(library.XLP, NORMAL_BETA_SPREAD, ETF_SIZE),
+                create_penny(library.XLU, LOW_BETA_SPREAD, ETF_SIZE),
                 create_penny(library.RSP, ETF_SPREAD, ETF_SIZE)
                 ]
 
 STOCK_SIZE = 12
-stock_pennies = [create_penny(library.AMZN, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.HD, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.DIS, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.PG, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.KO, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.PM, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.NEE, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.DUK, STOCK_SPREAD, STOCK_SIZE),
-                 create_penny(library.SO, STOCK_SPREAD, STOCK_SIZE)]
+stock_pennies = [create_penny(library.AMZN, HIGH_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.HD, HIGH_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.DIS, HIGH_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.PG, NORMAL_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.KO, NORMAL_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.PM, NORMAL_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.NEE, LOW_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.DUK, LOW_BETA_SPREAD, STOCK_SIZE),
+                 create_penny(library.SO, LOW_BETA_SPREAD, STOCK_SIZE)]
 
 
 
